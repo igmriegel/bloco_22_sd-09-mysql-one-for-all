49f31a89832c4c710356fec43ab923af7134beb0
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-06-08 04:22:11 -0300

    troca tab por espaço

--- desafio10.sql
+++ desafio10.sql
@@ -4,14 +4,14 @@ CREATE FUNCTION quantidade_musicas_no_historico(usr_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE quantity_musics INT;
-	SELECT 
-		COUNT(*)
-	FROM
-		SpotifyClone.reprodution_history AS h
-			LEFT JOIN
-		SpotifyClone.users AS u ON h.user_id = u.user_id
-	WHERE
-		u.user_id = usr_id INTO quantity_musics;
+    SELECT 
+        COUNT(*)
+    FROM
+        SpotifyClone.reprodution_history AS h
+            LEFT JOIN
+        SpotifyClone.users AS u ON h.user_id = u.user_id
+    WHERE
+        u.user_id = usr_id INTO quantity_musics;
     RETURN quantity_musics;
 END $$
 
